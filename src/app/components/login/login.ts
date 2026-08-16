a73959f388d9d3cea366f43d6b424f86ab29e916
ManojAR1252 <manoj89406@gmail.com>
2026-08-16 18:17:15 +0530

    Update login credentials check in Login component

--- src/app/components/login/login.ts
+++ src/app/components/login/login.ts
@@ -1,22 +1,31 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+
 @Component({
   selector: 'app-login',
+  standalone: true,
   imports: [FormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
+
   username = '';
   password = '';
+
   constructor(private router: Router) {}
+
   login() {
-    if (this.username === 'manoj' && this.password === 'manoj123') {
+
+    if (
+      this.username === 'manoj' &&
+      this.password === '123'
+    ) {
       this.router.navigate(['/dashboard']);
+    } else {
+      alert('Invalid Credentials');
     }
-    else {
-      alert('Invalid username or password');
-    }
+
   }
 }
