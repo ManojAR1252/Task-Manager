38b11a42d34d47d6f0c584dcd65d6856db7f2d0b
ManojAR1252 <manoj89406@gmail.com>
2026-08-16 18:16:13 +0530

    Enhance Dashboard component with task selection
    Added TaskForm import and implemented task selection functionality.

--- src/app/components/dashboard/dashboard.ts
+++ src/app/components/dashboard/dashboard.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
 import { TaskList } from '../task-list/task-list';
+import { TaskForm } from '../task-form/task-form';
+import { Task } from '../../models/task.model';
+
 @Component({
   selector: 'app-dashboard',
-  imports: [TaskList],
+  standalone: true,
+  imports: [TaskList, TaskForm],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css'
 })
-export class Dashboard {}
+export class Dashboard {
+
+  selectedTask: Task | null = null;
+
+  editTask(task: Task) {
+    this.selectedTask = task;
+  }
+
+  clearSelection() {
+    this.selectedTask = null;
+  }
+
+}
